f4ac19b648cd6a36b7d12616feb1c183b62ccbae
Emrah IŞIK <ssemrah@outlook.com>
2024-07-30 13:29:46 +0300

    newadded

--- app/NewsCard/NewsCardStyle.js
+++ app/NewsCard/NewsCardStyle.js
@@ -1,10 +1,14 @@
-import { StyleSheet,Dimensions } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 export default StyleSheet.create({
-    container:{backgroundColor:'white',margin:10,borderRadius:10},
-    image:{height:Dimensions.get('window').height / 4,borderTopLeftRadius:5,borderTopRightRadius:5,},
-    title:{fontWeight:'bold',fontSize:18},
-    description:{marginTop:3},
-    inner_container:{padding:5},
-author:{fontStyle:'italic',textAlign:"right"},
-banner_image:{height:Dimensions.get('window').height /5}
-})
+  container: { backgroundColor: "white", margin: 10, borderRadius: 10 },
+  image: {
+    height: Dimensions.get("window").height / 4,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+  },
+  title: { fontWeight: "bold", fontSize: 18 },
+  description: { marginTop: 3 },
+  inner_container: { padding: 5 },
+  author: { fontStyle: "italic", textAlign: "right" },
+  banner_image: { height: Dimensions.get("window").height / 5 },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,9 @@
-import { Stack} from "expo-router";
-import { StatusBar, View } from 'react-native';
+import { Stack } from "expo-router";
+import { StatusBar, View } from "react-native";
 export default function RootLayout() {
   return (
-
-
-    <Stack >
-      <Stack.Screen name="index" options={{headerShown:false}}/>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
